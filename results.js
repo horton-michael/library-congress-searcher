7d2d9ef4c063e37a3dd29c86089cedadda24c475
Taylor <taysully94@gmail.com>
2023-09-01 14:00:18 -0400

    adds some structure to both js pages with some pseudo coding

---
+++ results.js
@@ -0,0 +1,18 @@
+//DOM DEPENDENCIES================
+//create desired id and class tags and target them into variables
+
+//DATA============================
+
+
+//FUNCTIONS=======================
+//copy functions necessary for search onto this page for additional searches
+
+//render search results 
+
+
+
+//USER INTERACTIONS===============
+//target event - when user clicks on desired search result, links to loc.gov page?
+
+
+//INITIALIZATIONS================
